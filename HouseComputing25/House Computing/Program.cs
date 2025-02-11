ae34001905d24243b286664aa1f8391a264b1912
EndmostArcher <william.j.buckley@outlook.com>
2025-02-11 19:11:57 +0000

    Added [give player] and [game intro]

--- HouseComputing25/House Computing/Program.cs
+++ HouseComputing25/House Computing/Program.cs
@@ -7,7 +7,23 @@ void TypeWriter(string text)
     }
     Console.WriteLine();
 }
+
+// NEW CODE
+List<string> GivePlayer(string newItem, int manyItems, List<string> newInv)
+{
+    for(int i = manyItems; i >= 0; i--)
+    {
+        newInv.Add(newItem);
+    }
+    return newInv;
+}
+
+// END OF NEW CODE
+
+
 Console.WriteLine("╔═╗╔═╗    ╔╗               ╔╗            ╔═══╗                 \r\n║║╚╝║║    ║║              ╔╝╚╗           ║╔═╗║                 \r\n║╔╗╔╗║╔╗╔╗║║ ╔══╗╔══╗ ╔══╗╚╗╔╝╔══╗╔═╗    ║║ ╚╝╔══╗ ╔╗╔╗╔══╗╔══╗\r\n║║║║║║║║║║║║ ║╔═╝╚ ╗║ ║══╣ ║║ ║╔╗║║╔╝    ║║╔═╗╚ ╗║ ║╚╝║║╔╗║║══╣\r\n║║║║║║║╚╝║║╚╗║╚═╗║╚╝╚╗╠══║ ║╚╗║║═╣║║     ║╚╩═║║╚╝╚╗║║║║║║═╣╠══║\r\n╚╝╚╝╚╝╚══╝╚═╝╚══╝╚═══╝╚══╝ ╚═╝╚══╝╚╝     ╚═══╝╚═══╝╚╩╩╝╚══╝╚══╝\r\n                                                               \r\n                                                               ");
+
+
 Dictionary<string, int> attributes = new Dictionary<string, int>
 {
     ["Stamina"] = 100,
@@ -15,4 +31,14 @@ Dictionary<string, int> attributes = new Dictionary<string, int>
     ["Thirst"] = 100,
     ["Health"] = 100
 };
+
+
+
 List<string> inventory = new List<string>();
+
+
+void GameIntro()
+{
+    Console.WriteLine(""); // Add Intro Story Here
+
+}
