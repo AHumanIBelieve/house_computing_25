1ded72c1f082230891ff5d97886a9166814ff0c5
EndmostArcher <william.j.buckley@outlook.com>
2025-02-11 14:21:54 +0000

    Updated it: added attributes and inventory

--- HouseComputing25/House Computing/Program.cs
+++ HouseComputing25/House Computing/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello child");
+/*void TypeWriter(string text)
+{
+    foreach (char c in text)
+    {
+
+    }
+}*/
+
+Dictionary<string, int> attributes = new Dictionary<string, int>
+{
+    ["Stamina"] = 100,
+    ["Hunger"] = 100,
+    ["Thirst"] = 100,
+    ["Health"] = 100
+};
+List<string> inventory = new List<string>();
